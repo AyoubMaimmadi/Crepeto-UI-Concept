047ceffb2cae7eaed679dbc7664778522bd484ec
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-19 23:53:45 +0000

    new

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -5,17 +5,17 @@ export const getResources = (url, setData) => {
   fetch(`http://localhost:3090${url}`)
     .then((response) => response.json())
     .then((data) => {
-      setData(data);
+      setData(data)
     })
-    .catch((err) => console.error(err));
-};
+    .catch((err) => console.error(err))
+}
 
 // Make POST requests using passed url and data
 export const createResource = async (url, body) => {
   await fetch(`http://localhost:3090${url}`, {
-    method: "POST",
-    body: JSON.stringify(body)
+    method: 'POST',
+    body: JSON.stringify(body),
   }).then((response) => {
-    return response.json();
-  });
-};
+    return response.json()
+  })
+}
