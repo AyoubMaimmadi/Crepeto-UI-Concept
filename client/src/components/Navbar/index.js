ecae66de882b1a0cf68e7f14723d1794e87c6152
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-20 14:43:10 +0000

    new

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
-import Paper from "@material-ui/core/Paper";
-import { makeStyles } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import HomeIcon from "@material-ui/icons/Home";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import PeopleIcon from "@material-ui/icons/People";
-import MenuBookIcon from "@material-ui/icons/MenuBook";
-import LocalShippingIcon from "@material-ui/icons/LocalShipping";
-import LocationCityIcon from "@material-ui/icons/LocationCity";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from 'react'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import HomeIcon from '@material-ui/icons/Home'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import PeopleIcon from '@material-ui/icons/People'
+import MenuBookIcon from '@material-ui/icons/MenuBook'
+import LocalShippingIcon from '@material-ui/icons/LocalShipping'
+import LocationCityIcon from '@material-ui/icons/LocationCity'
+import { Link, useLocation } from 'react-router-dom'
 
 const useStyles = makeStyles({
   root: {
-    flexGrow: 1
-  }
-});
+    flexGrow: 1,
+  },
+})
 
 function Navbar() {
-  const classes = useStyles();
-  const location = useLocation();
-  const [value, setValue] = useState(location.pathname);
+  const classes = useStyles()
+  const location = useLocation()
+  const [value, setValue] = useState(location.pathname)
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    setValue(newValue)
+  }
 
   return (
     <Paper square className={classes.root}>
@@ -38,48 +38,48 @@ function Navbar() {
         <Tab
           icon={<HomeIcon />}
           component={Link}
-          to={"/"}
+          to={'/'}
           value="/"
           label="HOME"
         />
         <Tab
           icon={<ShoppingCartIcon />}
           component={Link}
-          to={"/products"}
+          to={'/products'}
           value="/products"
           label="PRODUCTS"
         />
         <Tab
           icon={<PeopleIcon />}
           component={Link}
-          to={"/customers"}
+          to={'/customers'}
           value="/customers"
           label="CUSTOMERS"
         />
         <Tab
           icon={<MenuBookIcon />}
           component={Link}
-          to={"/orders"}
+          to={'/orders'}
           value="/orders"
           label="ORDERS"
         />
         <Tab
           icon={<LocalShippingIcon />}
           component={Link}
-          to={"/suppliers"}
+          to={'/suppliers'}
           value="/suppliers"
           label="SUPPLIERS"
         />
         <Tab
           icon={<LocationCityIcon />}
           component={Link}
-          to={"/locations"}
-          value="/locations"
-          label="LOCATIONS"
+          to={'/employees'}
+          value="/employees"
+          label="Employees"
         />
       </Tabs>
     </Paper>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Customers from '../pages/customers'
 import Home from '../pages/home'
-import Locations from '../pages/locations'
+import Employees from '../pages/employees'
 import Orders from '../pages/orders'
 import Products from '../pages/products'
 import Suppliers from '../pages/suppliers'
@@ -12,7 +12,7 @@ const AppRouter = () => {
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/customers" component={Customers} />
-      <Route path="/locations" component={Locations} />
+      <Route path="/employees" component={Employees} />
       <Route path="/orders" component={Orders} />
       <Route path="/products" component={Products} />
       <Route path="/suppliers" component={Suppliers} />
