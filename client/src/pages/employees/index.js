a11aa7ea3d3bf8780483c5e7e2f127076010e686
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-20 14:44:43 +0000

    new

--- client/src/pages/employees/index.js
+++ client/src/pages/employees/index.js
@@ -2,24 +2,24 @@ import React, { useState, useEffect } from 'react'
 import PageContainer from '../../components/PageContainer'
 import AppTable from '../../components/AppTable'
 
-function Locations() {
-  const [locations, setLocations] = useState([])
+function Employees() {
+  const [employees, setEmployees] = useState([])
 
-  // Get all locations when the page is rendered
+  // Get all Employees when the page is rendered
   useEffect(() => {
     fetch('http://localhost:3090/customers/geographic-info')
       .then((response) => response.json())
       .then((data) => {
-        setLocations(data)
+        setEmployees(data)
       })
       .catch((err) => console.error(err))
   }, [])
 
   return (
     <PageContainer>
-      <h2>Locations</h2>
-      <AppTable tableData={locations} />
+      <h2>Employees</h2>
+      <AppTable tableData={employees} />
     </PageContainer>
   )
 }
-export default Locations
+export default Employees
