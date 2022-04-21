f2c897add5b155059a63521fa16c9ab5831de105
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-21 00:39:31 +0000

    new

---
+++ server/controllers/employee.js
@@ -0,0 +1,76 @@
+const pool = require('./pool')
+
+// Get all employees in the database
+exports.getEmployees = (req, res) => {
+  pool.query(`SELECT * FROM employee ORDER BY employee_id;`, (err, results) => {
+    if (err) {
+      throw err
+    }
+
+    res.status(200).json(results.rows)
+  })
+}
+
+// Get a employee's info based on the given id
+exports.getEmployee = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  pool.query(
+    `SELECT * FROM employee WHERE employee_id=${id};`,
+    (err, results) => {
+      if (err) {
+        throw err
+      }
+
+      res.status(200).json(results.rows)
+    }
+  )
+}
+
+// View 5: Get all addresses from employees and suppliers for logistical purposes
+exports.getGeographicInfo = (req, res) => {
+  pool.query(
+    `SELECT address FROM employee
+    UNION
+    SELECT address FROM supplier;`,
+    (err, results) => {
+      if (err) {
+        throw err
+      }
+
+      res.status(200).json(results.rows)
+    }
+  )
+}
+
+exports.getActiveEmployeesInfo = (req, res) => {
+  pool.query(
+    `SELECT DISTINCT name, phone, email, salary FROM employee;`,
+    (err, results) => {
+      if (err) {
+        throw err
+      }
+
+      res.status(200).json(results.rows)
+    }
+  )
+}
+
+// Add a new employee to the database
+exports.addEmployee = (req, res) => {
+  const { name, phone, email, address } = req.body
+
+  pool.query(
+    `INSERT INTO employee(name, phone, email, address, salary) 
+    VALUES('${name}', '${phone}', '${email}', '${address}', '${salary}')
+    RETURNING name, phone, email, address, salary;`,
+    (err, results) => {
+      if (err) {
+        throw err
+      }
+
+      // Return the newly created employee
+      res.status(201).send(results.rows)
+    }
+  )
+}
