cb0ac323faa1f9ff6ed6ac00e095edf729d50acb
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-25 15:42:13 +0000

    new

--- server/controllers/billing.js
+++ server/controllers/billing.js
@@ -1,1 +1,58 @@
 const pool = require('./pool')
+
+// get the mothly billing from /billing/:id
+exports.getMonthlyBilling = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  pool.query(
+    `SELECT * FROM billing WHERE MonthyBilling_id=${id}`,
+    (err, results) => {
+      if (err) {
+        throw err
+      }
+      res.status(200).json(results.rows)
+    }
+  )
+}
+
+// get mothly expenses from /billing/:id
+exports.getMonthlyExpense = (req, res) => {
+  const id = parseInt(req.params.id)
+  pool.query(
+    `SELECT expense FROM billing WHERE MonthyBilling_id=${id}`,
+    (err, results) => {
+      if (err) {
+        throw err
+      }
+      res.status(200).json(results.rows)
+    }
+  )
+}
+
+// get mothly revenue from /billing/:id
+exports.getMonthlyRevenue = (req, res) => {
+  const id = parseInt(req.params.id)
+  pool.query(
+    `SELECT revenue FROM billing WHERE MonthyBilling_id=${id}`,
+    (err, results) => {
+      if (err) {
+        throw err
+      }
+      res.status(200).json(results.rows)
+    }
+  )
+}
+
+//get profit from /billing/:id
+exports.getMonthlyProfit = (req, res) => {
+  const id = parseInt(req.params.id)
+  pool.query(
+    `SELECT profit FROM billing WHERE MonthyBilling_id=${id}`,
+    (err, results) => {
+      if (err) {
+        throw err
+      }
+      res.status(200).json(results.rows)
+    }
+  )
+}
