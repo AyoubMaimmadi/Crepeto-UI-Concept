a6725b6c64f269b2c458d322f00a81953b7416e8
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-25 15:29:32 +0000

    new

---
+++ client/src/pages/billing/index.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react'
+import PageContainer from '../../components/PageContainer'
+import AppTable from '../../components/AppTable'
+
+function Employees() {
+  const [billing, setBilling] = useState([])
+
+  // Get all Employees when the page is rendered
+  useEffect(() => {
+    fetch('http://localhost:3090/billing')
+      .then((response) => response.json())
+      .then((data) => {
+        setBilling(data)
+      })
+      .catch((err) => console.error(err))
+  }, [])
+
+  return (
+    <PageContainer>
+      <h2>Billing</h2>
+      <AppTable tableData={billing} />
+    </PageContainer>
+  )
+}
+export default Employees
