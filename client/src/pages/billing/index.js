bb4bb5c46604eca6557958c4585525a2188d8958
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-26 18:17:50 +0000

    new

--- client/src/pages/billing/index.js
+++ client/src/pages/billing/index.js
@@ -6,7 +6,7 @@ import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Divider from '@material-ui/core/Divider'
-import { getResources, createResource } from '../../helpers'
+import { getResources } from '../../helpers'
 
 const useStyles = makeStyles({
   dateField: {
